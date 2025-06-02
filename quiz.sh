5d6d95cebfa980d230e9fb71d9084b0f31295033
Anna Kowalska <anna.2.kowalska@student.uj.edu.pl>
2025-06-02 10:42:21 +0200

    Pytania

--- quiz.sh
+++ quiz.sh
@@ -37,15 +37,24 @@ ask_question() {
     echo "To poprawna odpowiedź"
     score=$((score + 1))
   else
-    echo "To zła odpowiedź"
+    echo "To zła odpowiedź" 
     echo "KONIEC GRY"
     echo "Twój wynik: $score"
     exit
   fi
 }
-ask_question "Ile to 2 + 2?" "3" "4" "5" "6" "B"
+ask_question "Ile to jest 10 angstremów (Å)?" "0,1nm" "0,01µm" "1nm" "10^-10m" "C"
 ask_question "Jaki jest symbol chemiczny wody?" "H2O" "O2" "CO2" "HCl" "A"
-
+ask_question "Jaka jest najdłuższa faza cyklu komórkowego?" "G2" "G1" "S" "M" "B"
+ask_question "Stężenie kinaz białkowych zależnych od cyklin (Cdk)..." "Zmienia się w zależności od fazy cyklu." "Rośnie pod czas mitozy." "Rośnie w trakcie interfazy." "Nie zależy od fazy cyklu komórkowego." "D"
+ask_question "Stadium mitozy po rozpadzie otoczki jądrowej, gdy chromosomy przyłączają się do wrzeciona to:" "profaza" "anafaza" "prometafaza" "metafaza" "C"
+ask_question "Które kompleksy translokacyjne współpracują ze sobą tworząc kanały do wnętrza mitochodrium?" "MIM 1 + SAM" "TIM22 + TIM23" "TOM + TIM22" "TIM23 + TOM" "D"
+ask_question "Która sekwencja sygnałowa kieruje białko do jądra komórkowego?" "NLS" "PTS1" "SRP" "KDEL" "A"
+ask_question "Wskaż fałszywe sformułowanie dotyczące funkcji peroksysomów." "Biorą udział w metabolizmie cholesterolu i kwasów żółciowych." "Detoksykacja alkoholu w wątrobie poprzez cytochrom P450" "Rozkładanie nadtlenku wodoru" "Synteza niektórych lipidów" "B"
+ask_question "Wokół centrosomu koncentrycznie rozchodzą się..." "Mikrofilamenty" "Mikrotubule" "Septyny" "Filamenty pośrednie" "B"
+ask_question "Nukleazy to enzymy które:" "Denaturują DNA" "Hydrolizują wiązania peptydowe" "Nacinają wiązanie fosfodiestrowe w kwasach nukleinowych" "Rozplatają dwuniciowe kwasy nukleinowe" "C"
+ask_question "U jakiego gatunku w błonach komórkowch będzie więcej fosfolipidów nienasyconych?" "Wąż pustynny" "Bakteria termofilna" "Ryba arktyczna" "Niedźwiedź polarny" "C"
+ask_question "Co przedstawia schemat Jabłońskiego?" "Cykl życiowy fluoroforu" "Szlak transportu elektronów w mitochondrium" "Przemiany energetyczne cząsteczek po absorpcji fotonu" "Przeskoki energetyczne kwasów nukleinowych" "C" 
 echo "Twój końcowy wynik: $score"
 
 
